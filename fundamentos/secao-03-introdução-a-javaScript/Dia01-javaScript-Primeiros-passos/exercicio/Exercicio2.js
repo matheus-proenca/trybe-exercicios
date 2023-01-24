ac510d6a6d8ee898c7549a671d43b787a458e6a1
matheus proença <matheusproenca2020@hotmail.com>
2023-01-24 09:47:20 -0300

    Exercicio2

---
+++ fundamentos/secao-03-introdução-a-javaScript/Dia01-javaScript-Primeiros-passos/exercicio/Exercicio2.js
@@ -0,0 +1,23 @@
+const num1 = 9;
+const num2 = 7;
+const num3 = 5;
+let result;
+
+if ((num1 % 2) == 0 || (num2 % 2) == 0 || (num3 % 2) == 0) {
+  result = true;
+} else {
+  result = false;
+}
+
+console.log(result);
+
+
+if ((num1 % 2) == 1 || (num2 % 2) == 1 || (num3 % 2) == 1) {
+  result = true;
+} else {
+  result = false;
+}
+
+console.log(result);
+
+
