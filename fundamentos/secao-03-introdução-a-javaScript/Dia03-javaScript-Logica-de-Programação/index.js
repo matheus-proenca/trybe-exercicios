bdca7ecc32b27efc674acc2fc904c08526110db4
matheus proença <matheusproenca2020@hotmail.com>
2023-01-25 18:26:28 -0300

    Exercicio 02

--- fundamentos/secao-03-introdução-a-javaScript/Dia03-javaScript-Logica-de-Programação/index.js
+++ fundamentos/secao-03-introdução-a-javaScript/Dia03-javaScript-Logica-de-Programação/index.js
@@ -6,3 +6,40 @@ for (let index = 0; index < word.length; index += 1) {
 }
 
 console.log(reverseWord);
+
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+
+let biggestWord = array[0];
+let smallestWord = array[0];
+
+for (let index = 0; index < array.length; index += 1) {
+  if (array[index].length > biggestWord.length) {
+    biggestWord = array[index];
+  }
+}
+
+for (let index = 0; index < array.length; index += 1) {
+  if (array[index].length < smallestWord.length) {
+    smallestWord = array[index];
+  }
+}
+
+console.log(biggestWord);
+console.log(smallestWord);
+
+
+let biggestPrimeNumber = 0;
+
+for (let currentNumber = 2; currentNumber <= 50; currentNumber += 1) {
+  let isPrime = true;
+  for (let currentDivisor = 2; currentDivisor < currentNumber; currentDivisor += 1) {
+    if (currentNumber % currentDivisor === 0) {
+      isPrime = false;
+    }
+  }
+  if (isPrime) {
+    biggestPrimeNumber = currentNumber;
+  }
+}
+
+console.log(biggestPrimeNumber);
